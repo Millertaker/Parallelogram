1301a53afecb979326ea1f3fe11d8cd3d8fa49b2
miller.gonzalez <miller.gonzalez@salesforce.com>
2018-01-06 23:20:34 -0500

    add on cnavas click event

--- src/js/main/canvas.js
+++ src/js/main/canvas.js
@@ -1,17 +1,27 @@
 import throttle from '../helpers/throttle';
 
 const Canvas = function(el, holder){
-  var gl = el.getContext("webgl");
+  var gl = el.getContext("2d");
+  
   const onResize = function(){
     el.width = holder.offsetWidth;
-    console.log(el.width)
   };
+
+  const onClick = function(e){
+    console.log(e.offsetX, e.offsetY);
+  }
+
 
   const initElement = function(){
     
     el.height = 800;
     el.width = holder.offsetWidth;
-    window.addEventListener('resize', throttle(onResize, 100))
+    window.addEventListener('resize', throttle(onResize, 100));
+    el.addEventListener('click', onClick);
+
+    gl.moveTo(0,0);
+    gl.lineTo(200,100);
+    gl.stroke();
   };
 
   initElement();
