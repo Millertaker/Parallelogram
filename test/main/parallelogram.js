b51194a0f6a540437cef9cd3acd9b962a13bd2b4
miller.gonzalez <miller.gonzalez@salesforce.com>
2018-01-09 22:46:32 -0500

    Parallelogram coors done

--- test/main/parallelogram.js
+++ test/main/parallelogram.js
@@ -21,11 +21,22 @@ describe('Parallelogram object test', () => {
     assert.equal(myParallelogram.lineColor,  null, 'a empty parallelogram doesnt has a line corlor');
   });
 
-  it('parallelogram should exists as empty object', () => {
-    var myParallelogram = parallelogram([0,0],[10,10], [45,45], '#FFFFFF', gl);
+  it('parallelogram should 4 coordinates', () => {
+    var myParallelogram = parallelogram( [ 705 , 352 ] , [ 792 , 218 ],  [ 947 , 224 ] , '#FFFFFF', gl);
+    var pointD = myParallelogram.draw();
 
-    assert.equal(typeof myParallelogram, 'object', 'the object exists');
-    assert.equal(myParallelogram.lineColor,  null, 'a empty parallelogram doesnt has a line corlor');
+    assert.equal(myParallelogram.pointA[0], 705, 'PointA x exists');
+    assert.equal(myParallelogram.pointA[1], 352, 'PointA y exists');
+
+    assert.equal(myParallelogram.pointB[0], 792, 'PointB x exists');
+    assert.equal(myParallelogram.pointB[1], 218, 'PointB y exists');
+
+    assert.equal(myParallelogram.pointC[0], 947, 'PointC x exists');
+    assert.equal(myParallelogram.pointC[1], 224, 'PointC y exists');
+
+    assert.equal(pointD[0], 860, 'PointD x exists');
+    assert.equal(pointD[1], 358, 'PointD y exists');
+    
   });
   
 });
